069646602579521eaac7e7234129eeb87391e2db
Tech-athirst <153635694+Tech-athirst@users.noreply.github.com>
2025-06-02 22:37:40 +0530

    Update chess0.v

--- chess0.v
+++ chess0.v
@@ -1,1 +1,14 @@
-
+module sr_flipflop (
+    input clk,
+    input s,
+    input r,
+    output reg q
+);
+    always @(posedge clk)
+        case ({s, r})
+            2'b00: q <= q;
+            2'b01: q <= 0;
+            2'b10: q <= 1;
+            2'b11: q <= 1'bx; // Invalid state
+        endcase
+endmodule

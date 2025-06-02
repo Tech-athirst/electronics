07853bec897b7fdc0d0929812177787db7d6fa13
Tech-athirst <153635694+Tech-athirst@users.noreply.github.com>
2025-06-02 22:46:36 +0530

    Update chess0.v

--- chess0.v
+++ chess0.v
@@ -1,14 +1,48 @@
-module sr_flipflop (
-    input clk,
-    input s,
-    input r,
+module dff(
+    input clk, reset,
+    input d,
     output reg q
 );
     always @(posedge clk)
-        case ({s, r})
-            2'b00: q <= q;
-            2'b01: q <= 0;
-            2'b10: q <= 1;
-            2'b11: q <= 1'bx; // Invalid state
-        endcase
+        if (reset)
+            q <= 0;
+        else
+            q <= d;
 endmodule
+module tff (
+    input clk, t,
+    output reg q
+);
+    always @(posedge clk)
+        if (t)
+            q <= ~q;
+endmodule
+module jkff (
+    input clk,
+    input j, k,
+    output reg q
+);
+    always @(posedge clk) begin
+        case ({j, k})
+            2'b00: q <= q;     // No change
+            2'b01: q <= 0;     // Reset
+            2'b10: q <= 1;     // Set
+            2'b11: q <= ~q;    // Toggle
+        endcase
+    end
+endmodule
+module srff (
+    input clk,
+    input s, r,
+    output reg q
+);
+    always @(posedge clk) begin
+        if (s & r)
+            q <= 1'bx;  // undefined
+        else if (s)
+            q <= 1;
+        else if (r)
+            q <= 0;
+    end
+endmodule
+
